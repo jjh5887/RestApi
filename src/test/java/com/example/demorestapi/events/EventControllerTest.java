3c1171b8941f791fdcdde7dd2d008d202c75df6c
jjh5887 <wjdrnjsgh159@naver.com>
2021-07-25 22:55:57 +0900

    테스트용 DB 설정 분리

--- src/test/java/com/example/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/example/demorestapi/events/EventControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -39,6 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTest {
 
     @Autowired
