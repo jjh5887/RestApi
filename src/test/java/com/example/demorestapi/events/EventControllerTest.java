50c780ba1b1236e0262f3ced58e9bf6b477c2922
jjh5887 <wjdrnjsgh159@naver.com>
2021-08-17 22:16:34 +0900

    테스트 코드 리팩토링

--- src/test/java/com/example/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/example/demorestapi/events/EventControllerTest.java
@@ -1,24 +1,13 @@
 package com.example.demorestapi.events;
 
-
-import com.example.demorestapi.common.RestDocsConfiguration;
+import com.example.demorestapi.common.BaseControllerTest;
 import com.example.demorestapi.common.TestDescription;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -36,22 +25,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTest {
+public class EventControllerTest extends BaseControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
-    @Autowired
-    ObjectMapper objectMapper;
-    @Autowired
     EventRepository eventRepository;
-    @Autowired
-    ModelMapper modelMapper;
+
     @Autowired
     private WebApplicationContext wac;
 
